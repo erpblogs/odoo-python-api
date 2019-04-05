b8a7c5ea38fa3547a67639fb386a2ab00bc7a2f5
QUANG Trinh Van <quang.trinhvan@vti.com.vn>
2019-04-05 14:33:20 +0700

    add odoo config & get

---
+++ _odoo_conf.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os.path
+import inspect
+
+def get_odoo_config_path():
+    directory_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    return os.path.join(directory_path, 'odoo_config.txt')
+
+def get_odoo_config():
+    open_file = open(get_odoo_config_path(), 'r')
+    config = open_file.read().splitlines()
+    res = dict()
+    for line in config:
+        data = line.split('=')
+        res[data[0]] = data[1]
+    open_file.close()
+
+    return res
